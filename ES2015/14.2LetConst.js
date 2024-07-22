d668bcdf3988dee56c2d39acd925f693e1943d05
AJ <skyhighzed@gmail.com>
2024-07-22 13:13:21 -0400

    14.2 Let Const

---
+++ ES2015/14.2LetConst.js
@@ -0,0 +1,10 @@
+//var PI = 3.14;
+//PI = 42; // stop me from doing this!
+
+const PI = 3.14;
+const pi = 42;
+
+//1. You can redeclare and reassign var but you can only reassign let. Var is function scope while let is block scope.
+//2. Const you cant redeclare or reassign and is also block scope.
+//3. You can reassign let but not const.
+//4. Explains how variables are intialized right away even if their values are declared later.
