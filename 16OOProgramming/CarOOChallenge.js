5a9f17d48069b5fc312990c94c74c5cec3672e73
AJ <skyhighzed@gmail.com>
2024-08-05 09:16:45 -0400

    Car OO Challenge

---
+++ 16OOProgramming/CarOOChallenge.js
@@ -0,0 +1,55 @@
+// Vehicle class
+class Vehicle {
+    constructor(make, model, year) {
+      this.make = make;
+      this.model = model;
+      this.year = year;
+    }
+  
+    honk() {
+      return "Beep.";
+    }
+  
+    toString() {
+      return `The vehicle is a ${this.make} ${this.model} from ${this.year}.`;
+    }
+  }
+  
+  // Car class that extends Vehicle
+  class Car extends Vehicle {
+    constructor(make, model, year) {
+      super(make, model, year);
+      this.numWheels = 4;
+    }
+  }
+  
+  // Motorcycle class that extends Vehicle
+  class Motorcycle extends Vehicle {
+    constructor(make, model, year) {
+      super(make, model, year);
+      this.numWheels = 2;
+    }
+  
+    revEngine() {
+      return "VROOM!!!";
+    }
+  }
+  
+  // Garage class
+  class Garage {
+    constructor(capacity) {
+      this.vehicles = [];
+      this.capacity = capacity;
+    }
+  
+    add(vehicle) {
+      if (!(vehicle instanceof Vehicle)) {
+        return "Only vehicles are allowed in here!";
+      }
+      if (this.vehicles.length >= this.capacity) {
+        return "Sorry, we're full.";
+      }
+      this.vehicles.push(vehicle);
+      return "Vehicle added!";
+    }
+  }
